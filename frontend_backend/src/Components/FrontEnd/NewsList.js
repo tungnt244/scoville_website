3a05880ec45bbef480061b5061368dc76d33d7d6
daiphat1808e <phat@sc0ille.com>
2017-08-17 10:42:20 +0900

    CMS part

--- frontend_backend/src/Components/FrontEnd/NewsList.js
+++ frontend_backend/src/Components/FrontEnd/NewsList.js
@@ -2,6 +2,34 @@ import React from 'react'
 import '../../Styles/NewsListStyle.css'
 
 export default class NewsList extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            articles: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get(api_url +'/news/brief').then(response => {
+            this.setState({
+                articles: response.data
+            })
+        }).catch(error => {
+            console.log('error: ', error)
+        })
+    }
+
+    renderAllSummary(){
+        let articles = this.state.articles
+        let temp = []
+        for(let i=0; i<articles.length; i++){
+            console.log(i, JSON.stringify(articles[i], null, 4))
+            temp.push(<CMSSummary key={articles[i].id} article={articles[i]}/>)
+        }
+        return temp
+    }
+
     render(){
         return(
             <div>
@@ -12,31 +40,7 @@ export default class NewsList extends React.Component {
                             <br/><br/>
                         </section>
                     </div>
-                    <div>
-                        <p className="date-text text-left ">2017.07.21</p>
-                        <a href="" className="text-left blue-text">
-                        {`AIを活用したタクシー配車アプリの実用実験を今夏に開始、早くも兆…`}</a>
-                    </div>
-                    <div>
-                        <p className="date-text text-left">2017.06.21</p>
-                        <a href="" className="text-left blue-text">
-                        {`タローがコーポレートサイトのリニューアルを開始`}</a>
-                    </div>
-                    <div>
-                        <p className="date-text text-left">2017.04.15</p>
-                        <a href="" className="text-left blue-text">
-                        {`AIを活用したタクシー配車アプリの実用実験を今夏に開始`}</a>
-                    </div>
-                    <div>
-                        <p className="date-text text-left">2017.04.15</p>
-                        <a href="" className="text-left blue-text">
-                        {`AIを活用したタクシー配車アプリの実用実験を今夏に開始`}</a>
-                    </div>
-                    <div>
-                        <p className="date-text text-left">2017.04.15</p>
-                        <a href="" className="text-left blue-text">
-                        {`AIを活用したタクシー配車アプリの実用実験を今夏に開始`}</a>
-                    </div>
+                    {this.renderAllSummary()}
                     <tr>
                         <th>
                             <button className="btn num-btn blue-number">
@@ -78,3 +82,30 @@ export default class NewsList extends React.Component {
         )
     }
 }
+
+// <div>
+//                         <p className="date-text text-left ">2017.07.21</p>
+//                         <a href="" className="text-left blue-text">
+//                         {`AIを活用したタクシー配車アプリの実用実験を今夏に開始、早くも兆…`}</a>
+//                     </div>
+//                     <div>
+//                         <p className="date-text text-left">2017.06.21</p>
+//                         <a href="" className="text-left blue-text">
+//                         {`タローがコーポレートサイトのリニューアルを開始`}</a>
+//                     </div>
+//                     <div>
+//                         <p className="date-text text-left">2017.04.15</p>
+//                         <a href="" className="text-left blue-text">
+//                         {`AIを活用したタクシー配車アプリの実用実験を今夏に開始`}</a>
+//                     </div>
+//                     <div>
+//                         <p className="date-text text-left">2017.04.15</p>
+//                         <a href="" className="text-left blue-text">
+//                         {`AIを活用したタクシー配車アプリの実用実験を今夏に開始`}</a>
+//                     </div>
+//                     <div>
+//                         <p className="date-text text-left">2017.04.15</p>
+//                         <a href="" className="text-left blue-text">
+//                         {`AIを活用したタクシー配車アプリの実用実験を今夏に開始`}</a>
+//                     </div>
+
